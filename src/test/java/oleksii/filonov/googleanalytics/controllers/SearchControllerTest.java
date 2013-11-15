6d580f3ee7dfc1c6f67ee0997e44e7f1df904fee
Oleksii Filonov <oleksii.filonov@gmail.com>
2013-11-15 23:00:14 +0200

    Fix SearchControllerTest

--- src/test/java/oleksii/filonov/googleanalytics/controllers/SearchControllerTest.java
+++ src/test/java/oleksii/filonov/googleanalytics/controllers/SearchControllerTest.java
@@ -1,22 +1,35 @@
 package oleksii.filonov.googleanalytics.controllers;
 
+import static org.mockito.Mockito.when;
+import static org.springframework.test.util.ReflectionTestUtils.setField;
+
+import java.util.Collections;
+
+import oleksii.filonov.googleanalytics.domainmodel.Product;
 import oleksii.filonov.googleanalytics.domainmodel.ProductStorage;
 
 import org.junit.Before;
 import org.junit.Test;
-import org.springframework.test.util.ReflectionTestUtils;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.runners.MockitoJUnitRunner;
 import org.springframework.ui.ExtendedModelMap;
 import org.springframework.ui.Model;
 
 
+@RunWith(MockitoJUnitRunner.class)
 public class SearchControllerTest {
 
 	private SearchController searchController;
 
+	@Mock
+	private ProductStorage productStorage;
+
 	@Before
 	public void setUp() {
 		this.searchController = new SearchController();
-		ReflectionTestUtils.setField(this.searchController, "productStorage", new ProductStorage());
+		when(this.productStorage.getProducts()).thenReturn(Collections.<Product>emptyList());
+		setField(this.searchController, "productStorage", this.productStorage);
 	}
 
 	@Test
