f39041683de09987525cbb88e773718d9b6b0114
Oleksii Filonov <Oleksii.Filonov@travisperkins.co.uk>
2013-11-26 19:51:57 +0200

    Add test skeleton for create session listener

---
+++ src/test/java/oleksii/filonov/googleanalytics/listeners/CreateCartListenerTest.java
@@ -0,0 +1,15 @@
+package oleksii.filonov.googleanalytics.listeners;
+
+import org.junit.Before;
+
+public class CreateCartListenerTest {
+	
+	private CreateCartListener testInstance;
+	
+	@Before
+	public void setUp() {
+		testInstance = new CreateCartListener();
+		
+	}
+
+}
